41981cd2ae59e58ab8bb2d381096309368b459a8
Victor Ikuomola <vheecthurikuomola@gmail.com>
2024-05-02 19:05:26 +0100

    chore: clean

--- src/bin/expressions.rs
+++ src/bin/expressions.rs
@@ -6,6 +6,11 @@ enum Access {
 }
 
 fn main() {
+    // EXPRESSIONS
+    // Rust is an expression-based language
+    // this means that most things are evaluated and return some value
+    // expresssion value coalesce to a single point and as a result, expressions can be used for nesting loginc
+
     // secret file: admisn only
     let access_level = Access::Guest;
     let can_access_file: bool = match access_level {
